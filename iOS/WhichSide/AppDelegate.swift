8eecd2cdffa2de6f9276083590627c6edecb491d
Fabian Canas <fcanas@gmail.com>
2015-07-05 21:51:26 -0400

    Initial iOS project structure

---
+++ iOS/WhichSide/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WhichSide
+//
+//  Created by Fabian Canas on 7/5/15.
+//  Copyright (c) 2015 Fabián Cañas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+    
+}
---
+++ iOS/WhichSide/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WhichSide
+//
+//  Created by Fabian Canas on 7/5/15.
+//  Copyright (c) 2015 Fabián Cañas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
